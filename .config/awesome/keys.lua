1f2775a98c9a0b131e94700dc26482ed931e9a08
sproott <david.hrabe12@seznam.cz>
2021-08-31 16:04:16 +0200

    awesome

--- .config/awesome/keys.lua
+++ .config/awesome/keys.lua
@@ -18,20 +18,10 @@ awful.key({MODKEY}, "s", hotkeys_popup.show_help, {
 }), awful.key({MODKEY, "Shift"}, "q", awesome.quit, {
   description = "quit awesome",
   group = "awesome"
-}), awful.key({MODKEY}, "x", function()
-  awful.prompt.run {
-      prompt = "Run Lua code: ",
-      textbox = awful.screen.focused().mypromptbox.widget,
-      exe_callback = awful.util.eval,
-      history_path = awful.util.get_cache_dir() .. "/history_eval"
-  }
-end, {
-  description = "lua execute prompt",
-  group = "awesome"
 }), awful.key({MODKEY}, "Return", function()
   awful.spawn(TERMINAL)
 end, {
-  description = "open a TERMINAL",
+  description = "open a terminal",
   group = "launcher"
 }), awful.key({MODKEY}, "r", function()
   awful.screen.focused().mypromptbox:run()
