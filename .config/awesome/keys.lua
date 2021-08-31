4239a0e7a7fe546e5cbb728b70e4798f543f3cf8
sproott <david.hrabe12@seznam.cz>
2021-08-31 16:13:48 +0200

    awesome client keys

--- .config/awesome/keys.lua
+++ .config/awesome/keys.lua
@@ -5,7 +5,7 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 
 require("globals")
 
-local keys = {global_keys = {}}
+local keys = {global_keys = {}, client_keys = {}}
 
 -- General Awesome keys
 keys.global_keys = gears.table.join(keys.global_keys,
@@ -24,9 +24,9 @@ end, {
   description = "open a terminal",
   group = "launcher"
 }), awful.key({MODKEY}, "r", function()
-  awful.screen.focused().mypromptbox:run()
+  awful.util.spawn("dmenu_run")
 end, {
-  description = "run prompt",
+  description = "run dmenu",
   group = "launcher"
 }))
 
@@ -242,4 +242,62 @@ end, {
   group = "custom"
 }))
 
+keys.client_keys = gears.table.join(keys.client_keys, awful.key({MODKEY}, "f", function(c)
+  c.fullscreen = not c.fullscreen
+  c:raise()
+end, {
+  description = "toggle fullscreen",
+  group = "client"
+}), awful.key({MODKEY, "Shift"}, "c", function(c)
+  c:kill()
+end, {
+  description = "close",
+  group = "client"
+}), awful.key({MODKEY, "Control"}, "space", awful.client.floating.toggle, {
+  description = "toggle floating",
+  group = "client"
+}), awful.key({MODKEY, "Control"}, "Return", function(c)
+  c:swap(awful.client.getmaster())
+end, {
+  description = "move to master",
+  group = "client"
+}), awful.key({MODKEY}, "o", function(c)
+  c:move_to_screen()
+end, {
+  description = "move to screen",
+  group = "client"
+}), awful.key({MODKEY}, "t", function(c)
+  c.ontop = not c.ontop
+end, {
+  description = "toggle keep on top",
+  group = "client"
+}), awful.key({MODKEY}, "n", function(c)
+  -- The client currently has the input focus, so it cannot be
+  -- minimized, since minimized clients can't have the focus.
+  c.minimized = true
+end, {
+  description = "minimize",
+  group = "client"
+}), awful.key({MODKEY}, "m", function(c)
+  c.maximized = not c.maximized
+  c:raise()
+end, {
+  description = "(un)maximize",
+  group = "client"
+}), awful.key({MODKEY, "Control"}, "m", function(c)
+  c.maximized_vertical = not c.maximized_vertical
+  c:raise()
+end, {
+  description = "(un)maximize vertically",
+  group = "client"
+}), awful.key({MODKEY, "Shift"}, "m", function(c)
+  c.maximized_horizontal = not c.maximized_horizontal
+  c:raise()
+end, {
+  description = "(un)maximize horizontally",
+  group = "client"
+}))
+
 root.keys(keys.global_keys)
+
+return keys
--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -23,7 +23,7 @@ require("awful.hotkeys_popup.keys")
 require("globals")
 
 -- Keybindings
-require("keys")
+local keys = require("keys")
 
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
@@ -172,64 +172,6 @@ client.connect_signal("request::default_mousebindings", function()
     end)})
 end)
 
-client.connect_signal("request::default_keybindings", function()
-    awful.keyboard.append_client_keybindings({awful.key({MODKEY}, "f", function(c)
-        c.fullscreen = not c.fullscreen
-        c:raise()
-    end, {
-        description = "toggle fullscreen",
-        group = "client"
-    }), awful.key({MODKEY, "Shift"}, "c", function(c)
-        c:kill()
-    end, {
-        description = "close",
-        group = "client"
-    }), awful.key({MODKEY, "Control"}, "space", awful.client.floating.toggle, {
-        description = "toggle floating",
-        group = "client"
-    }), awful.key({MODKEY, "Control"}, "Return", function(c)
-        c:swap(awful.client.getmaster())
-    end, {
-        description = "move to master",
-        group = "client"
-    }), awful.key({MODKEY}, "o", function(c)
-        c:move_to_screen()
-    end, {
-        description = "move to screen",
-        group = "client"
-    }), awful.key({MODKEY}, "t", function(c)
-        c.ontop = not c.ontop
-    end, {
-        description = "toggle keep on top",
-        group = "client"
-    }), awful.key({MODKEY}, "n", function(c)
-        -- The client currently has the input focus, so it cannot be
-        -- minimized, since minimized clients can't have the focus.
-        c.minimized = true
-    end, {
-        description = "minimize",
-        group = "client"
-    }), awful.key({MODKEY}, "m", function(c)
-        c.maximized = not c.maximized
-        c:raise()
-    end, {
-        description = "(un)maximize",
-        group = "client"
-    }), awful.key({MODKEY, "Control"}, "m", function(c)
-        c.maximized_vertical = not c.maximized_vertical
-        c:raise()
-    end, {
-        description = "(un)maximize vertically",
-        group = "client"
-    }), awful.key({MODKEY, "Shift"}, "m", function(c)
-        c.maximized_horizontal = not c.maximized_horizontal
-        c:raise()
-    end, {
-        description = "(un)maximize horizontally",
-        group = "client"
-    })})
-end)
-
 -- }}}
 
 -- {{{ Rules
@@ -244,6 +186,7 @@ ruled.client.connect_signal("request::rules", function()
             raise = true,
             screen = awful.screen.preferred,
             placement = awful.placement.no_overlap + awful.placement.no_offscreen,
+            keys = keys.client_keys,
         }
     }
 
