47a40cdaaf46639529226fe2627a677193d71c0b
sproott <david.hrabe12@seznam.cz>
2021-08-31 19:15:51 +0200

    💅 default awesome theme for later lit modifications

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -39,7 +39,8 @@ end)
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+local theme_path = string.format('%s/.config/awesome/themes/%s/theme.lua', os.getenv('HOME'), 'default')
+beautiful.init(theme_path)
 
 -- {{{ Tag
 -- Table of layouts to cover with awful.layout.inc, order matters.
