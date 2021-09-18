df005e8dc031621b9e81679a659f3755d12b3fbd
sproott <david.hrabe12@seznam.cz>
2021-09-18 22:57:38 +0200

    add missing nvim configs

---
+++ .config/nvim/lua/lsp.lua
@@ -0,0 +1,16 @@
+require('treesitter')
+
+-- Defaults
+local lsp = require('lspconfig')
+
+-- lsp.cssls.setup {}
+lsp.graphql.setup {}
+lsp.hls.setup {
+  settings = {
+    haskell = {
+      formattingProvider = 'stylish-haskell'
+    }
+  }
+}
+-- lsp.html.setup {}
+-- lsp.jsonls.setup {}
