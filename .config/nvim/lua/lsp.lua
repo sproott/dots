1c4b01c95f268c0c6c435bb33a8d056161fb9419
sproott <david.hrabe12@seznam.cz>
2021-09-25 14:33:40 +0200

    lua language server

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -6,16 +6,44 @@ local coq = require('coq')
 local enable_snippets = coq.lsp_ensure_capabilities
 
 lsp.bashls.setup(enable_snippets {})
-lsp.ccls.setup{}
+lsp.ccls.setup {}
 -- lsp.cssls.setup {}
-lsp.graphql.setup {}
-lsp.hls.setup {
+lsp.efm.setup {
+  init_options = {documentFormatting = true},
+  fileTypes = {'lua'},
   settings = {
-    haskell = {
-      formattingProvider = 'stylish-haskell'
+    rootMarkers = {'.git/'},
+    languages = {
+      lua = {
+        {
+          formatCommand = 'lua-format -i --indent-width=2 --double-quote-to-single-quote',
+          formatStdin = true
+        }
+      }
     }
   }
 }
+lsp.graphql.setup {}
+lsp.hls.setup {settings = {haskell = {formattingProvider = 'stylish-haskell'}}}
 -- lsp.html.setup {}
 -- lsp.jsonls.setup {}
+
+local sumneko_binary_path = 'lua-language-server'
+local sumneko_root_path = '/usr/share/lua-language-server'
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
+require'lspconfig'.sumneko_lua.setup {
+  cmd = {sumneko_binary_path, '-E', sumneko_root_path .. '/main.lua'},
+  settings = {
+    Lua = {
+      runtime = {version = 'LuaJIT', path = runtime_path},
+      diagnostics = {globals = {'vim'}},
+      workspace = {library = vim.api.nvim_get_runtime_file('', true)}
+    }
+  }
+}
+
 lsp.texlab.setup(enable_snippets {})
