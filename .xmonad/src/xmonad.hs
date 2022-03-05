af1e08a67bff5c928c008499f6602bbe791b1b2c
sproott <david.hrabe12@seznam.cz>
2022-03-05 19:15:46 +0100

    XMonad screenshot key

--- .xmonad/src/xmonad.hs
+++ .xmonad/src/xmonad.hs
@@ -45,6 +45,8 @@ myKeys conf@XConfig {XMonad.modMask = modm} =
       ((modm, xK_p), spawn "rofi -show"),
       -- lock the screen
       ((modm .|. controlMask, xK_s), spawn "lockscreen"),
+      -- lock the screen
+      ((modm .|. shiftMask, xK_Print), spawn "selection_screenshot"),
       -- emoji picker
       ((modm, xK_period), spawn "emoji"),
       -- close focused window
